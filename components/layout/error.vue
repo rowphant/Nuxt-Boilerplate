1aca7175fff44c3b2ec4341e08c4d1079d9de426
Robert Metzner <info@robertmetzner.com>
2025-03-26 18:08:23 +0100

    Add error page

---
+++ components/layout/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen place-items-center place-content-center">
+    <div class="place-items-center place-content-center">
+      <div class="font-light text-xl">Oopsie!</div>
+      <div class="text-8xl text-accent font-bold my-4">Error 404</div>
+      <div class="text-3xl font-light text-center">
+        The site you're looking for<br />is not available.
+      </div>
+      <div class="font-light opacity-50">Go out and read a book.</div>
+      <!-- <div class="pt-4">
+      <ThemeSwitch />
+    </div> -->
+    </div>
+  </div>
+</template>
---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+import Error from "@/components/layout/error.vue";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <NuxtLayout name="404">
+    <Error />
+    <!-- <h2>{{ error.statusCode }}</h2>
+    <button @click="handleError">Clear errors</button> -->
+  </NuxtLayout>
+</template>
