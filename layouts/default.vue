a745538b52b87e9c64b0969e2410cc55fac82aea
Robert Metzner <info@robertmetzner.com>
2025-03-26 03:18:45 +0100

    🎨

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,6 +14,6 @@
 </template>
 
 <script setup lang="ts">
-import Header from "@/components/layout/Header.vue";
-import Footer from "@/components/layout/Footer.vue";
+import Header from "@components/layout/Header.vue";
+import Footer from "@components/layout/Footer.vue";
 </script>
