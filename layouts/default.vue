290740612bc708f78f1ba6e06dd2dc668ba14411
Robert Metzner <info@robertmetzner.com>
2025-03-26 18:19:23 +0100

    Fix syntax

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,6 +14,6 @@
 </template>
 
 <script setup lang="ts">
-import Header from "../components/layout/header.vue";
-import Footer from "../components/layout/footer.vue";
+import Header from "@/components/layout/header.vue";
+import Footer from "@/components/layout/footer.vue";
 </script>
