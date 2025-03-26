69de9dc2bb2f17f589afd33573c917de38185e3d
Robert Metzner <info@robertmetzner.com>
2025-03-26 03:45:21 +0100

    no message

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  ssr: false,
+  // devtools: { enabled: true },
+  // ssr: false,
   app: {
     pageTransition: { name: "page", mode: "out-in" },
     layoutTransition: { name: "layout", mode: "out-in" },
