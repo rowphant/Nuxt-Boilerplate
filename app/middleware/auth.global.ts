f1dd6284dee8a4e146c4b713eea05730efd59fee
Robert Metzner <info@robertmetzner.com>
2025-05-19 01:49:10 +0200

    🐛 Fix redirect for unauthenticated users

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,10 +2,13 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const { isAuthenticated } = useAuth();
   const { user, fetchUser } = useUser();
 
-  if (to.meta.requiresAuth && !isAuthenticated.value) {
+  // If the route requires authentication and the user is not authenticated,
+  if (to.meta.requiresAuth && ! await isAuthenticated.value) {
     return navigateTo("/login");
   }
   
+  // If the user is authenticated but not yet fetched, fetch the user data
+  // This is to ensure that the user data is available for the authenticated routes
   if (await isAuthenticated.value && !user.value) {
     await fetchUser();
   }
