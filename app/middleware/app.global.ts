9449fe3a076f21b2b834c95d4f4a2352f34d585b
Robert Metzner <info@robertmetzner.com>
2025-05-22 13:59:54 +0200

    Implement global middleware for app settings initialization

--- app/middleware/app.global.ts
+++ app/middleware/app.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  console.log("Global Middleware for settings");
+  console.log("Global Middleware for app settings");
   const globalStore = useGlobalStore();
 
   if (!globalStore.settings) {
