07b8365807abe12bcfaf50c12561807e072aad28
Robert Metzner <info@robertmetzner.com>
2025-05-23 14:41:53 +0200

    Refactor error handling components and improve error display

---
+++ app/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <h2>{{ error.statusCode }}</h2>
+    <button @click="handleError">Clear errors</button> -->
+    <Error :error="error" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+import Error from '~/components/layout/Error.vue';
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+})
+
+// const handleError = () => clearError({ redirect: '/' })
+</script>
---
+++ app/layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center pt-20">
+    <h1 class="text-4xl font-bold text-red-600">{{ error.statusCode }}</h1>
+    <p class="text-lg mt-4">{{ error.statusMessage }}</p>
+    <NuxtLink to="/" class="mt-6 inline-block text-blue-500 underline"
+      >Zurück zur Startseite</NuxtLink
+    >
+  </div>
+</template>
+
+<script setup>
+defineProps({ error: Object });
+</script>
